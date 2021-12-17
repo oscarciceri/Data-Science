9fa46fd98ab55b488294f39fa3f869141ed9ba1d
“oscarciceri” <oscarcicericoral@gmail.com>
2021-12-17 19:28:41 -0300

    dispersion

--- Plots/plot_dispersion.py
+++ Plots/plot_dispersion.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot as plt
 import seaborn as sns
 import json
 import math 
 import os
 import random
-
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
 
 name="dispersion_dataset.in"
@@ -17,6 +18,50 @@ num_cols = len(f.readline().split('	'))
 f.seek(0)
 lines = f.read().splitlines()
 f.close()
+
 lines.pop(0)
 size = len(lines)
-print(size)
+
+
+date = []
+apple = []
+google = []
+bankAmerica = []
+
+# for i in range(len(lines)):
+for i in range(50):
+    data = lines[i].split('	')
+    date.append(str(data[0].replace(',', '.')))
+    apple.append(str(data[1].replace(',', '.')))
+    google.append(str(data[2].replace(',', '.')))
+    bankAmerica.append(str(data[3].replace(',', '.')))
+print(google[1])
+
+
+
+
+
+
+# Fixing random state for reproducibility
+np.random.seed(19680801)
+# N = len(google)
+# colors = np.random.rand(N)
+# area = (10 * np.random.rand(N))**2  # 0 to 15 point radii
+
+
+len(apple)
+len(google)
+
+# matplotlib.axes.Axes.set_xscale(1, 'linear')
+
+
+x = np.array(apple)
+y = np.array(google)
+
+
+plt.scatter(x, y, alpha=0.5, cmap=cm.Paired, marker='.')
+plt.xlabel("Google") # Text for X-Axis
+plt.ylabel("Apple") # Text for Y-Axis
+plt.title("Relationship between Google and Applle")
+
+plt.show()
